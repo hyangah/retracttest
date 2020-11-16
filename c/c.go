0b512b965696cff5792edb420b7458145187d6a6
Hana <hyangah@gmail.com>
2020-11-16 17:34:06 -0500

    c: built with good weather

---
+++ c/c.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "github.com/hyangah/retracttest/b"
+
+func main() {
+	fmt.Println(b.V)
+}
---
+++ c/go.mod
@@ -0,0 +1,5 @@
+module github.com/hyangah/retracttest/c
+
+go 1.15
+
+require github.com/hyangah/retracttest/b v1.0.1
